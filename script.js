446935117d31007debbf7b751c1b45172f0e169f
EJCPMJP <142831189+EJCPMJP@users.noreply.github.com>
2025-09-06 15:32:27 -0300

    Adicionar carrossel e ajustar layout (#3)
    Beleza

--- script.js
+++ script.js
@@ -43,3 +43,15 @@ window.addEventListener('resize', () => {
 resize();
 createStars();
 updateStars();
+
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  }
+});
